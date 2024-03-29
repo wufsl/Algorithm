63cd6fde4f325c66f50d1a276d294249f65fddea
wufsl <142829847+wufsl@users.noreply.github.com>
2024-03-30 02:13:45 +0900

    [Bronze I] Title: 팰린드롬수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,10 +1,17 @@
+list1=[]
+list2=[]
 while True:
     arr=input("")
     if (arr=='0'):
         break
     else:
-        arr2=arr[::-1]
-        if(arr==arr2):
-            print('yes')
-        else:
-            print('no')
+        list1.append(arr)
+for j in range(len(list1)):
+    for i in list1:
+        list2.append(i[::-1])
+        
+for k in range(len(list1)):
+    if(list1[k]==list2[k]):
+        print('yes')
+    else:
+        print('no')
